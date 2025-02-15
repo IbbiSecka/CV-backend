c98bdf0168c559ddff6a8e65ac6d9c8f8729e412
IbbiSecka <s355423@oslomet.no>
2025-02-15 21:04:44 +0100

    Add project files.

---
+++ CV/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace CV.Data
+{
+    public class DataContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public DataContext(DbContextOptions<DataContext> options, IConfiguration configuration): base (options)
+        {
+            _configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = _configuration.GetConnectionString("DefaultConnectionString");
+            optionsBuilder.UseNpgsql(connectionString);
+
+            optionsBuilder.LogTo(message => Debug.WriteLine(message)); // Log SQL queries
+        }
+    }
+}
---
+++ CV/Models/Ibbi.cs
@@ -0,0 +1,12 @@
+﻿namespace CV.Models
+{
+    public class Ibbi
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string DOB { get; set; }
+        public string Img { get; set; }
+
+    }
+}
